b2c4e638b0ad1a84158be65a2b612f683e6514fd
steve6472 <steve64959@gmail.com>
2021-01-02 13:21:23 +0100

    World shader now uses correct shading

--- src/main/resources/shaders/world.fs
+++ src/main/resources/shaders/world.fs
@@ -2,8 +2,10 @@
 
 in vec4 vColor;
 in vec2 vTexture;
+in vec3 vNormal;
 
 uniform sampler2D atlas;
+uniform sampler2D normal;
 uniform float shade;
 
 out vec4 fragColor;
@@ -14,7 +16,14 @@ void main()
 
 	if (orig.a == 0) discard;
 
+    const float AMBIENT = 0.5;
+    const float XFAC = -0.15;
+    const float ZFAC = 0.05;
+
+    float yLight = (1.0 + vNormal.y) * 0.5;
+    float light = yLight * (1.0 - AMBIENT) + vNormal.x * vNormal.x * XFAC + vNormal.z * vNormal.z * ZFAC + AMBIENT;
+
     fragColor = orig * vColor;
 
-    fragColor.rgb = fragColor.rgb * shade;
+    fragColor.rgb = fragColor.rgb * shade * light;
 }
--- target/classes/shaders/world.vs
+++ target/classes/shaders/world.vs
@@ -3,6 +3,7 @@
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec4 color;
 layout(location = 2) in vec2 texture;
+layout(location = 3) in vec3 normal;
 
 uniform mat4 transformation;
 uniform mat4 projection;
@@ -10,13 +11,13 @@ uniform mat4 view;
 
 out vec4 vColor;
 out vec2 vTexture;
+out vec3 vNormal;
 
 void main()
 {
     vColor = color;
     vTexture = texture;
+    vNormal = normal;
 
-	vec4 worldPosition = transformation * vec4(position, 1.0);
-
-	gl_Position = projection * view * worldPosition;
+    gl_Position = projection * view * transformation * vec4(position, 1.0);
 }
